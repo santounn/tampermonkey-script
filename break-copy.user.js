1f7ac8346d5ee6d318da4fa761ed518a7fa31747
santounn <106853695+santounn@users.noreply.github.com>
2022-06-04 10:44:11 +0800

    Create break-copy.user.js

---
+++ break-copy.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         break-copy
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  csdn代码复制!
+// @author       You
+// @match        https://blog.csdn.net/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.debug('tempermonkey hello world.');
+
+    document.body.contentEditable=true
+
+})();
